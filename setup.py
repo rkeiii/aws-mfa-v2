8f9d6d9e9af062cb7ca7a9c34e6b261738c63992
Ron Ellis <rkeiii@gmail.com>
2020-11-24 09:34:23 -0800

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aws-mfa-v2',
-    version='0.2.4',
+    version='0.2.5',
     description='Manage AWS MFA Security Credentials',
     long_description=long_description,
     long_description_content_type='text/markdown',
